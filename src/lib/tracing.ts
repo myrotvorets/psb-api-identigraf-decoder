2452a57920fe29ec0e11c3905603d49fab7d006a
Myrotvorets <support@myrotvorets.center>
2020-10-15 05:50:19 +0300

    Use AsyncHooksContextManager

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -2,8 +2,9 @@
 
 import opentelemetry from '@opentelemetry/api';
 import { NodeTracerProvider } from '@opentelemetry/node';
-import { SimpleSpanProcessor } from '@opentelemetry/tracing';
+import { BatchSpanProcessor } from '@opentelemetry/tracing';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+import { AsyncHooksContextManager } from '@opentelemetry/context-async-hooks';
 
 const provider = new NodeTracerProvider({
     plugins: {
@@ -16,13 +17,16 @@ const provider = new NodeTracerProvider({
     },
 });
 
+const contextManager = new AsyncHooksContextManager().enable();
+opentelemetry.context.setGlobalContextManager(contextManager);
+
 if (+(process.env.ENABLE_TRACING ?? 0) && process.env.ZIPKIN_ENDPOINT) {
     const zipkinExporter = new ZipkinExporter({
         url: process.env.ZIPKIN_ENDPOINT,
         serviceName: 'psb-api-identigraf-decoder',
     });
 
-    const zipkinProcessor = new SimpleSpanProcessor(zipkinExporter);
+    const zipkinProcessor = new BatchSpanProcessor(zipkinExporter);
     provider.addSpanProcessor(zipkinProcessor);
 }
 
