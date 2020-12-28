ac4a53a8633e2e105c9bd901c5d7b0d251730cd2
Myrotvorets <support@myrotvorets.center>
2020-12-28 19:57:07 +0200

    Update dependencies

--- src/lib/tracing.ts
+++ src/lib/tracing.ts
@@ -1,28 +1,23 @@
 /* istanbul ignore file */
 
-import { NodeTracerProvider } from '@opentelemetry/node';
-import { SimpleSpanProcessor } from '@opentelemetry/tracing';
-import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+import { EventEmitter } from 'events';
+import { OpenTelemetryConfigurator } from '@myrotvorets/opentelemetry-configurator';
 
-const provider = new NodeTracerProvider({
-    plugins: {
-        express: {},
-        http: {},
-        https: {},
-        knex: {
-            path: '@myrotvorets/opentelemetry-plugin-knex',
-        },
-    },
-});
-
-if (+(process.env.ENABLE_TRACING || 0) && process.env.ZIPKIN_ENDPOINT) {
-    const zipkinExporter = new ZipkinExporter({
-        url: process.env.ZIPKIN_ENDPOINT,
+if (+(process.env.ENABLE_TRACING || 0)) {
+    const configurator = new OpenTelemetryConfigurator({
         serviceName: 'psb-api-identigraf-decoder',
+        tracer: {
+            plugins: {
+                express: {},
+                http: {},
+                https: {},
+                knex: {
+                    path: '@myrotvorets/opentelemetry-plugin-knex',
+                },
+            },
+        },
     });
 
-    const zipkinProcessor = new SimpleSpanProcessor(zipkinExporter);
-    provider.addSpanProcessor(zipkinProcessor);
+    configurator.start().catch((e) => console.error('Failed to initialize OpenTelemetry:', e));
+    EventEmitter.defaultMaxListeners += 5;
 }
-
-provider.register();
