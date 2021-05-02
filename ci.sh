f708163e51f518900bd48f177f9df95775cef4dc
Unsung Hero <66092015+myrotvorets-team@users.noreply.github.com>
2021-05-02 03:38:21 +0300

    Delete ci.sh

--- ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p coverage
-touch test-report.xml
-chmod -R a+w test-report.xml coverage
-docker-compose -f docker-compose-ci.yml up --build --abort-on-container-exit --renew-anon-volumes
